6c7d3874d379e146f35f4674d521fd24bf26310b
reemake <safnikita@inbox.ru>
2021-12-10 19:45:39 +0300

    Added the ability to read data about contracts from CSV-file and add them to the repository

---
+++ src/main/java/csvReader/CsvReader.java
@@ -0,0 +1,96 @@
+package csvReader;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import contracts.*;
+import entities.Human;
+import repository.Repository;
+import java.io.*;
+
+/**
+ * Class which is describing methods for reading data from CSV-file
+ * @author Nikita Safonov, student of AMM VSU, 3rd year, 3rd group
+ * @see Repository
+ * @see Contract
+ * @see MobileConnectionContract
+ * @see InternetConnectionContract
+ * @see TelevisionContract
+ */
+public class CsvReader {
+
+    /** Variable storing the path to the file */
+    private String csvFilePath;
+    /** Separator that used to split the lines of the CSV-file */
+    private char separator;
+    /** Object of the Repository class, into which the contract data read from the CSV-file is adding */
+    private Repository repo;
+
+    /** Constructor with parameters for an object of the сsvReader class */
+    public CsvReader(String csvFilePath, char separator, Repository repo) {
+        this.csvFilePath = csvFilePath;
+        this.separator = separator;
+        this.repo = repo;
+    }
+
+    /**
+     * Method for reading data from CSV-file
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws CsvValidationException
+     */
+    public void readFromCSV() {
+        try (FileReader in = new FileReader(csvFilePath)) {
+            CSVParser parser = new CSVParserBuilder().withSeparator(separator).build();
+            CSVReader reader = new CSVReaderBuilder(in).withCSVParser(parser).withSkipLines(1).build();
+            if (reader == null) {
+                throw new FileNotFoundException(csvFilePath);
+            }
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                Human person = new Human(Integer.parseInt(nextLine[4]), nextLine[5], nextLine[7], nextLine[6], nextLine[8]);
+                if (nextLine[9].equals("Mobile")) {
+                    String[] mobileTariffPlan = nextLine[10].split(",");
+                    MobileConnectionContract contract = new MobileConnectionContract(Integer.parseInt(nextLine[0]), nextLine[2], nextLine[3], Integer.parseInt(nextLine[1]), person, Integer.parseInt(mobileTariffPlan[0]), Integer.parseInt(mobileTariffPlan[1]), Integer.parseInt(mobileTariffPlan[2]));
+                    repo.addContract(contract);
+                }
+                else if (nextLine[9].equals("Television")) {
+                    TelevisionContract contract = new TelevisionContract(Integer.parseInt(nextLine[0]), nextLine[2], nextLine[3], Integer.parseInt(nextLine[1]), person, nextLine[10]);
+                    repo.addContract(contract);
+                }
+                else if (nextLine[9].equals("Internet")) {
+                    InternetConnectionContract contract = new InternetConnectionContract(Integer.parseInt(nextLine[0]), nextLine[2], nextLine[3], Integer.parseInt(nextLine[1]), person, Integer.parseInt(nextLine[10]));
+                    repo.addContract(contract);
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCsvFilePath() {
+        return csvFilePath;
+    }
+
+    public void setCsvFilePath(String csvFilePath) {
+        this.csvFilePath = csvFilePath;
+    }
+
+    public char getSeparator() {
+        return separator;
+    }
+
+    public void setSeparator(char separator) {
+        this.separator = separator;
+    }
+
+    public static void main(String[] args) {
+        Repository repo = new Repository();
+        CsvReader reader = new CsvReader("C://Users/User/IdeaProjects/netcracker_labs/src/main/resources/contracts_information.csv",  ';', repo);
+        reader.readFromCSV();
+        System.out.println(repo.getContractByIndex(3));
+    }
+
+}
---
+++ src/test/java/csvReader/CsvReaderTest.java
@@ -0,0 +1,96 @@
+package csvReader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.Repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvReaderTest {
+
+    Repository repo;
+    CsvReader reader;
+
+    @BeforeEach
+    void setUp() {
+        repo = new Repository();
+        reader = new CsvReader("C://Users/User/IdeaProjects/netcracker_labs/src/main/resources/contracts_information.csv",  ';', repo);
+    }
+
+    @Test
+    void ifAllLinesBeenReadTest() {
+        reader.readFromCSV();
+        int expectedValue = 5;
+        int actualValue = repo.getSize();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void ifMobileContractBeenReadCorrectTest() {
+        reader.readFromCSV();
+        String expectedValue =
+                "Контракт на мобильную связь № 111\n" +
+                "ID: 1\n" +
+                "Дата начала контракта: 2020-01-01\n" +
+                "Дата окончания контракта: 2022-01-01\n" +
+                "Количество минут: 200\n" +
+                "Количество смс: 2000\n" +
+                "Траффик: 30 Гб\n" +
+                "\n" +
+                "Владелец контракта: \n" +
+                "ID: 1\n" +
+                "ФИО: Jack N.\n" +
+                "Дата рождения: 1995-02-01\n" +
+                "Пол: M\n" +
+                "Паспорт: 2222 222222\n" +
+                "Возраст: 26";
+        String actualValue = repo.getContractByIndex(0).toString();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void ifInternetContractBeenReadCorrectTest() {
+        reader.readFromCSV();
+        String expectedValue =
+                "Контракт на проводной интернет № 444\n" +
+                        "ID: 4\n" +
+                        "Дата начала контракта: 2017-03-03\n" +
+                        "Дата окончания контракта: 2022-03-03\n" +
+                        "Скорость интернета: 60 Mbps\n" +
+                        "\n" +
+                        "Владелец контракта: \n" +
+                        "ID: 4\n" +
+                        "ФИО: Anna F.\n" +
+                        "Дата рождения: 1987-06-17\n" +
+                        "Пол: F\n" +
+                        "Паспорт: 1111 111111\n" +
+                        "Возраст: 34";
+        String actualValue = repo.getContractByIndex(3).toString();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void ifTelevisionContractBeenReadCorrectTest() {
+        reader.readFromCSV();
+        String expectedValue =
+                "Контракт на цифровое телевидение № 222\n" +
+                        "ID: 2\n" +
+                        "Дата начала контракта: 2019-01-01\n" +
+                        "Дата окончания контракта: 2021-01-01\n" +
+                        "Пакет каналов: package1\n" +
+                        "\n" +
+                        "Владелец контракта: \n" +
+                        "ID: 2\n" +
+                        "ФИО: Clint B.\n" +
+                        "Дата рождения: 1991-02-02\n" +
+                        "Пол: M\n" +
+                        "Паспорт: 3333 333333\n" +
+                        "Возраст: 30";
+        String actualValue = repo.getContractByIndex(1).toString();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
